fbdec0ecfd40210654ed02b1e9522a95f210a3d8
axelblack4ever <145280362+axelblack4ever@users.noreply.github.com>
2025-03-16 10:31:48 +0100

    Cambiado el FAB de agregar tarea para que aparezca en todas las páginas de la aplicación

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,14 +1,33 @@
+// src/app/tabs/tabs.page.ts
 import { Component } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { 
+  IonTabs, 
+  IonTabBar, 
+  IonTabButton, 
+  IonIcon, 
+  IonLabel,
+  IonFab,
+  IonFabButton
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { todayOutline, listOutline, optionsOutline, barChartOutline } from 'ionicons/icons';
+import { todayOutline, listOutline, optionsOutline, barChartOutline, add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel]
+  imports: [
+    RouterLink,
+    IonTabs, 
+    IonTabBar, 
+    IonTabButton, 
+    IonIcon, 
+    IonLabel,
+    IonFab,
+    IonFabButton
+  ]
 })
 export class TabsPage {
   constructor() {
@@ -16,7 +35,8 @@ export class TabsPage {
       todayOutline,
       listOutline,
       optionsOutline,
-      barChartOutline
+      barChartOutline,
+      add
     });
   }
 }
